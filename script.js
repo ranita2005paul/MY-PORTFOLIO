bd74edd8cbd63ae355521bc58a235987c39489d2
RanitaPaul <ranita05paul@gmail.com>
2025-09-06 09:28:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Greeting popup when site loads
+window.onload = () => {
+  alert("Welcome to Ranita Paul's Portfolio 🚀");
+};
+
+// Contact form alert
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  alert("Your email app will now open. Please confirm and send your message ✅");
+});
